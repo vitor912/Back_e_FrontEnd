fb53782ade968330bca7b97ececf10924d4a84ee
Vitor Ribeiro <dev_vitor912@hotmail.com>
2022-10-04 22:55:05 -0300

    Colocando algumas fotos

--- Proeventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Proeventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,12 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos : any;
+  public eventos : any = [];
+  larguraImagem: number = 150;
+  margemImagem: number = 2;
+  exibirImagem: boolean = true;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
+  }
+
+  alterarImagem(){
+    this.exibirImagem = !this.exibirImagem;
   }
 
   public getEventos(): void {
