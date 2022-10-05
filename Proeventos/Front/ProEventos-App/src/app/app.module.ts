f8115b8c59752c18195ccd72217bd21915a3bf55
Vitor Ribeiro <dev_vitor912@hotmail.com>
2022-10-05 18:14:57 -0300

    Criando Filtros

--- Proeventos/Front/ProEventos-App/src/app/app.module.ts
+++ Proeventos/Front/ProEventos-App/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { NavComponent } from './nav/nav.component';
+
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
@@ -23,6 +25,7 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
