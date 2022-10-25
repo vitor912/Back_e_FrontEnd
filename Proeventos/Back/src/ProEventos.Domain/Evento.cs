4731bfc7fb35575847436cfa08f8a49e1844e314
Vitor Ribeiro <dev_vitor912@hotmail.com>
2022-10-25 18:00:17 -0300

    Iniciando a Class 'ProEventosContext'

--- Proeventos/Back/src/ProEventos.Domain/Evento.cs
+++ Proeventos/Back/src/ProEventos.Domain/Evento.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ProEventos.Domain
 {
@@ -15,8 +13,8 @@ namespace ProEventos.Domain
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
-        public IEnumerable<Lote> Lote { get; set; }
+        public IEnumerable<Lote> Lotes { get; set; }
         public IEnumerable<RedeSocial> RedesSociais { get; set; }
-
+        public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
     }
 }
--- Proeventos/Back/src/ProEventos.Domain/PalestranteEvento.cs
+++ Proeventos/Back/src/ProEventos.Domain/PalestranteEvento.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ProEventos.Domain
 {
-    class PalestranteEvento
+    public class PalestranteEvento
     {
+        public int PalestranteId { get; set; }
+        public Palestrante Palestrante { get; set; }
+        public int EventoId { get; set; }
+        public Evento Evento { get; set; }
     }
 }
